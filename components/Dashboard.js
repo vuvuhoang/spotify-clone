f1e308ab6674dfbf6780e058bcd771b88254dcde
vuvuhoang <vuvuhoang99@gmail.com>
2022-05-13 15:16:38 -0400

    fix width and margin auto

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -27,7 +27,7 @@ function Dashboard() {
   }
 
   return (
-    <main className="flex min-h-screen min-w-max bg-black lg:pb-24">
+    <main className="flex w-[1600px] m-auto min-h-screen min-w-max bg-black lg:pb-24">
         <Sidebar />
         <Body chooseTrack={chooseTrack} spotifyApi={spotifyApi} />
         <Right chooseTrack={chooseTrack} spotifyApi={spotifyApi} />
